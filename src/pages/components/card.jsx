88e72cfc5725621b0d473b09d82b3c3a005bd9b3
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2022-04-29 15:12:47 -0300

    atualizar

--- src/pages/components/card.jsx
+++ src/pages/components/card.jsx
@@ -6,12 +6,14 @@ import Images from "./images";
 function Card(props) {
   return (
     <main class={styles.cards}>
-      <section class={styles.cards_section}>
+      <section>
         <div class={styles.icon}>
           <img src={props.caminho} descricao={props.descricao} />
         </div>
         <p>{props.nome}</p>
-        <a url={props.link}> {props.chamar}</a>
+        <div class={styles.link}>
+          <a url={props.link}> {props.chamar}</a>
+        </div>
       </section>
     </main>
   );
